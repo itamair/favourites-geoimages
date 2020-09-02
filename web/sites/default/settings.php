fef0f901724d00344bd71126d2a81852bcf719f5
itamair <itamair@me.com>
2020-09-02 10:24:07 +0200

    settings ['config_sync_directory'] in '../config/sync'

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,5 +1,10 @@
 <?php
 
+// Set up a config sync directory.
+//
+// This is defined inside the read-only "config" directory, deployed via Git.
+$settings['config_sync_directory'] = '../config/sync';
+
 // #ddev-generated: Automatically generated Drupal settings file.
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
