786d3ce5f998522a16946a5cd9209f14b266732b
itamair <itamair@me.com>
2020-09-17 15:39:42 +0200

    settings.php file updated

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -5,6 +5,11 @@
 // This is defined inside the read-only "config" directory, deployed via Git.
 $settings['config_sync_directory'] = '../config/sync';
 
+// Local settings. These come last so that they can override anything.
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
+
 // #ddev-generated: Automatically generated Drupal settings file.
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
