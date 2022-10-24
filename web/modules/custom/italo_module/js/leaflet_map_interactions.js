4392a8ef16a366f74990bc8369d5434358792417
itamair <itamair@me.com>
2022-10-24 14:51:01 +0200

    italo_module update

--- web/modules/custom/italo_module/js/leaflet_map_interactions.js
+++ web/modules/custom/italo_module/js/leaflet_map_interactions.js
@@ -124,4 +124,57 @@
 
   };
 
+  Drupal.Leaflet.prototype.create_linestring = function(polyline) {
+    let latlngs = [];
+    for (let i = 0; i < polyline.points.length; i++) {
+      let latlng = new L.LatLng(polyline.points[i].lat, polyline.points[i].lon);
+      latlngs.push(latlng);
+    }
+    return new L.Polyline(latlngs);
+  };
+
+  Drupal.Leaflet.prototype.create_polygon = function(polygon) {
+    let latlngs = [];
+    for (let i = 0; i < polygon.points.length; i++) {
+      let latlng = new L.LatLng(polygon.points[i].lat, polygon.points[i].lon);
+      latlngs.push(latlng);
+    }
+    return new L.Polygon(latlngs);
+  };
+
+  Drupal.Leaflet.prototype.create_multipolygon = function(multipolygon) {
+    let polygons = [];
+    for (let x = 0; x < multipolygon.component.length; x++) {
+      let latlngs = [];
+      let polygon = multipolygon.component[x];
+      for (let i = 0; i < polygon.points.length; i++) {
+        let latlng = new L.LatLng(polygon.points[i].lat, polygon.points[i].lon);
+        latlngs.push(latlng);
+      }
+      polygons.push(latlngs);
+    }
+    return new L.Polygon(polygons);
+  };
+
+  // Override the original leaflet.drupal.js method not to use the
+  // L.PolygonClusterable object.
+  Drupal.Leaflet.prototype.create_multipoly = function(multipoly) {
+    let polygons = [];
+    for (let x = 0; x < multipoly.component.length; x++) {
+      let latlngs = [];
+      let polygon = multipoly.component[x];
+      for (let i = 0; i < polygon.points.length; i++) {
+        let latlng = new L.LatLng(polygon.points[i].lat, polygon.points[i].lon);
+        latlngs.push(latlng);
+      }
+      polygons.push(latlngs);
+    }
+    if (multipoly.multipolyline) {
+      return new L.Polyline(polygons);
+    }
+    else {
+      return new L.Polygon(polygons);
+    }
+  };
+
 })(jQuery, Drupal);
