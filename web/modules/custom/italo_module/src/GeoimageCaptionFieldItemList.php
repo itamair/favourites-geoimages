801c5c06131f27f1932eeb75781e06328ccf4cfc
itamair <itamair@me.com>
2022-11-19 18:15:19 +0100

    italo_module updated: - Added Geoimage Caption computed base field - css styling, over Olivero Taranto Viva custom theme.

---
+++ web/modules/custom/italo_module/src/GeoimageCaptionFieldItemList.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\italo_module;
+
+use Drupal\Core\Field\FieldItemList;
+use Drupal\Core\TypedData\ComputedItemListTrait;
+use Drupal\node\NodeInterface;
+
+/**
+ * Generates a GeoimageCaptionFieldItemList.
+ */
+class GeoimageCaptionFieldItemList extends FieldItemList {
+
+  use ComputedItemListTrait;
+
+  /**
+   * Whether the value has been calculated.
+   *
+   * @var bool
+   */
+  protected $isCalculated = FALSE;
+
+  /**
+   * {@inheritdoc}
+   *
+   * Generate the Value for the Geoimage Caption Field,
+   * only for the Geoimage Node Bundle.
+   */
+  protected function computeValue() {
+    if (!$this->isCalculated) {
+      $entity = $this->getEntity();
+      if ($entity instanceof NodeInterface && $entity->bundle() === "geoimage") {
+        /** @var \Drupal\node\NodeInterface $enity */
+        /** @var \Drupal\user\UserInterface $entity_author */
+        $entity_author = $entity->getOwner();
+        $forename = $entity_author->field_forename->value;
+        $surname = $entity_author->field_surname->value;
+        $value = $this->t('<div>@title <span>(©Credits: @forename @surname)</span></div>', [
+          '@title' => $entity->label(),
+          '@forename' => $forename,
+          '@surname' => $surname,
+        ]);
+        // $this->setValue($value);
+        $this->list[0] = $this->createItem(0, $value);
+        $this->isCalculated = TRUE;
+      }
+    }
+  }
+
+}
