5b513d7b2d9f1ac5fb0c10976c2b57cc37298807
itamair <itamair@me.com>
2022-12-28 23:43:39 +0100

    "drupal/uikit": "dev-3.x" required with taranto_viva_uikit starterkit generated

---
+++ web/themes/custom/taranto_viva_uikit/js/taranto_viva_uikit.theme.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Attaches behaviors for taranto_viva_uikit.
+ */
+
+(function ($) {
+
+  'use strict';
+
+  Drupal.behaviors.taranto_viva_uikit = {
+    attach: function () {
+    }
+  };
+})(jQuery);
