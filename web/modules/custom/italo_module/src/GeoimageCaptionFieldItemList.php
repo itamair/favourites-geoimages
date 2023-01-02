c1dfe96ef9a0801b37950693501325951df2b10e
itamair <itamair@me.com>
2023-01-02 20:34:23 +0100

    set 'geoimage_caption' computed field for Media entity ...

--- web/modules/custom/italo_module/src/GeoimageCaptionFieldItemList.php
+++ web/modules/custom/italo_module/src/GeoimageCaptionFieldItemList.php
@@ -5,6 +5,7 @@ namespace Drupal\italo_module;
 use Drupal\Core\Field\FieldItemList;
 use Drupal\Core\TypedData\ComputedItemListTrait;
 use Drupal\node\NodeInterface;
+use Drupal\Media\MediaInterface;
 
 /**
  * Generates a GeoimageCaptionFieldItemList.
@@ -29,8 +30,8 @@ class GeoimageCaptionFieldItemList extends FieldItemList {
   protected function computeValue() {
     if (!$this->isCalculated) {
       $entity = $this->getEntity();
-      $entity_bundles = ['geoimage', 'territorial_report'];
-      if ($entity instanceof NodeInterface && in_array($entity->bundle(), $entity_bundles)) {
+      $entity_bundles = ['geoimage'];
+      if ($entity instanceof MediaInterface && in_array($entity->bundle(), $entity_bundles)) {
         /** @var \Drupal\node\NodeInterface $entity */
         /** @var \Drupal\user\UserInterface $entity_author */
         $entity_author = $entity->getOwner();
@@ -38,15 +39,8 @@ class GeoimageCaptionFieldItemList extends FieldItemList {
         $surname = $entity_author->field_surname->value;
         switch ($entity->bundle()) {
           case 'geoimage':
-            $value = $this->t('<div class="geoimage-caption"><span class="geoimage-title">@title</span> - <span class="geoimage-credits">Credits: @forename @surname</span></div>', [
-              '@title' => $entity->label(),
-              '@forename' => $forename,
-              '@surname' => $surname,
-            ]);
-            break;
-
-          case 'territorial_report':
-            $value = $this->t('<div class="geoimage-caption"><span class="geoimage-credits">Credits: @forename @surname</span></div>', [
+            $value = $this->t('<div class="geoimage-caption"><span class="geoimage-address">@address</span> - <span class="geoimage-credits">Credits: @forename @surname</span></div>', [
+              '@address' => $entity->field_address ? $entity->field_address->value : '',
               '@forename' => $forename,
               '@surname' => $surname,
             ]);
